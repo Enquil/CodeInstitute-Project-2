5f847e8a4132751fec279a412876502b7d636275
Enquil <jim.e.olesen@gmail.com>
2022-07-02 12:07:30 +0000

    Calulate correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,8 +39,17 @@ function checkAnswer() {
 
 }
 
+/**
+ * gets numbers and calculates them based on what gametype is selected diectly from DOM
+ */
 function calculateCorrectAnswer() {
-    let correctAnswer = (num1+num2);
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"]
+    }
 }
 
 function incrementScore() {

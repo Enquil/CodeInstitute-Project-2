063048641acca6be8d11a24f9d0bb03a6c8c210c
Enquil <jim.e.olesen@gmail.com>
2022-07-03 12:05:24 +0000

    m

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,7 +81,7 @@ function displaySubtractQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "-";
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "*";

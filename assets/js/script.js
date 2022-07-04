c9727ddb4731864d33acfc367dd45e35c49b1f66
Enquil <jim.e.olesen@gmail.com>
2022-07-04 16:09:11 +0000

    i figured it outgit add .!

--- assets/js/script.js
+++ assets/js/script.js
@@ -102,7 +102,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivideQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = Math.max(operand1, operand2);
+    document.getElementById('operand2').textContent = Math.min(operand1, operand2);
     document.getElementById('operator').textContent = "/";
 }

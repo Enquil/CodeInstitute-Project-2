cfb786662babfe7987070fba9606b1c024d222de
Enquil <jim.e.olesen@gmail.com>
2022-07-13 20:30:04 +0000

    added some JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
+    // Set login to false on screen load 
+document.addEventListener('DOMContentLoaded'), function() {
+        
+    document.getElementById("clicker").addEventListener("click", Hello)
+    
+        
+}
 
+// check username and pass against object
+
+function Hello () {
+    console.log(getElementById(uname).innerText);
+}
---
+++ assets/js/test.js
@@ -0,0 +1,14 @@
+    // Set login to false on screen load 
+
+    document.addEventListener('DOMContentLoaded'), function() {
+        let 
+}
+
+
+// Login object, mock check
+
+let masterUser = {
+    uname: 'admin',
+    psw: '12345678',
+    locked: false,
+}

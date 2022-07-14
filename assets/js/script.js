9bc37a283b76d36fc933934cb604230cde352aa2
Enquil <jim.e.olesen@gmail.com>
2022-07-14 18:33:02 +0000

    start over

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,20 @@
 
+const users = ['admin', "jack", "jocko", "jordan",]
 
+const admin = {
+    username: "admin",
+    password: "hello",
+    name: "Cerbera",
+    year: "2022",
+}
+
+form = document.getElementById('handshake')
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit (event){
+    event.preventDefault();
+    let check1 = document.getElementById('uname')
+    if (users.includes(check1))
+        console.log("Nope")
+;
+}
--- assets/js/user-data.js
+++
@@ -1,9 +0,0 @@
-const meow = [
-    "admin0",
-    "gustav1",
-    "eric14",
-    "harald1",
-];
-
-console.log(meow[0]);
-

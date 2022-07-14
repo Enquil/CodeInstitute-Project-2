3bb1754d2dc54f66f130b0f94287fa3a744fe986
Enquil <jim.e.olesen@gmail.com>
2022-07-14 16:52:26 +0000

    structured user data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,2 @@
-    // Set login to false on screen load 
-document.addEventListener('DOMContentLoaded'), function() {
-        
-    document.getElementById("clicker").addEventListener("click", Hello)
-    
-        
-}
 
-// check username and pass against object
 
-function Hello () {
-    console.log(getElementById(uname).innerText);
-}
---
+++ assets/js/user-data.js
@@ -0,0 +1,9 @@
+const meow = [
+    "admin0",
+    "gustav1",
+    "eric14",
+    "harald1",
+];
+
+console.log(meow[0]);
+

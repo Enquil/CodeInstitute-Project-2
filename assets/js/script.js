dd5952d1f583104b99cc1c12daa17ae0489e22fd
Enquil <jim.e.olesen@gmail.com>
2022-07-15 18:38:28 +0000

    making mobile friendlyt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "history") {
+                alert("Hello! I am an alert box!!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
     }
-})
+
+    runGame("addition");
+
+});

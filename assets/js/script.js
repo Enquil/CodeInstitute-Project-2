5e005105ce8d2112827e5537206050240cc8985c
Enquil <jim.e.olesen@gmail.com>
2022-07-15 15:14:06 +0000

    made main-window

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
 
-const users = ['admin', "jack", "jocko", "jordan",]
-
-const admin = {
-    username: "admin",
-    password: "hello",
-    name: "Cerbera",
-    year: "2022",
-}
-
-form = document.getElementById('handshake')
-form.addEventListener('submit', handleSubmit);
-
-function handleSubmit (event){
-    event.preventDefault();
-    let check1 = document.getElementById('uname')
-    if (users.includes(check1))
-        console.log("Nope")
-;
-}
+    for (let button of buttons)
+})

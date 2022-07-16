f4573b3990e907b3d6be7c3c7096e83b2991c844
Enquil <jim.e.olesen@gmail.com>
2022-07-16 12:39:20 +0000

    made a menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,13 @@ let loggedIn = false;
 
 
 //control panel
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("click", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "newgame") {
                 newGame();
-            } else if (this.getAttribute("data-type") === "login") {
-                logIn();
+            } else if (this.getAttribute("data-type") === "credits") {
+                creditList();
             } else if (this.getAttribute("data-type") === "contact") {
                 contactMe();
             }
@@ -23,8 +23,6 @@ document.addEventListener("DOMContentLoaded", function() {
 //Function for making a newGame
 function newGame() {
     alert("moe");
-    document.getElementById("main-window").remove = "none";
-    document.getElementById("game-selector").add.class = "block";
 }
 
 //Login modal window

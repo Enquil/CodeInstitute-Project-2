2681d7a3c8b97c85d4c0b78a1f51ae0c017a2bf3
Jim Olesen <jim.e.olesen@gmail.com>
2022-07-17 10:58:57 +0200

    removed some javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,5 @@ document.getElementById("newgame").addEventListener("click", newGame);
 
 function newGame () {
     alert("hello");
-    document.getElementById("button-panel").classList.add("none");
-    document.getElementById("links-and-info").classList.add("none");
-    document.getElementById("category-picker").classList.remove("none");
 }
 

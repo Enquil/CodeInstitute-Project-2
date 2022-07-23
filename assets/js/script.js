91a95586854fc4570d1140533d676fdd1c1de344
Jim Olesen <102961268+Enquil@users.noreply.github.com>
2022-07-23 10:08:36 +0200

    added questions and function to id game category

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,40 @@
-document.getElementById("newgame").addEventListener("click", newGame);
+let buttons = document.getElementsByTagName("button");
 
-function newGame () {
-    alert("hello");
+for (let button of buttons) {
+    button.addEventListener("click", function() {
+        if (this.getAttribute("data-type") === "culture" || "history" || "science" || "sports");
+        category = this.getAttribute("data-type");
+            newGame(category);
+    });
 }
+
+function newGame(category) {
+    
+    if (category === "culture") {
+        cultureGame();
+    } else if (category === "history") {
+        alert("voof");
+    } else if (category === "science") {
+        alert("cluck");
+    } else if (category === "sports") {
+        alert("yuck");
+    } else
+    alert(`Unknown game type: ${category}`);
+}
+
+
+const questions = [
+    {id: 1, categories: [culture, history], answer: "Foo Fighters", wrong: ["nirvana", "thirty seconds to mars", "metallica", "meow"], 
+        question: "Dave Grohl founded what band?", difficulty: 1},
+
+    {id: 2, categories: [sports, history], answer: "Lionel Messi", wrong: ["christiano ronaldo", "luka modric", "michel platini", "tyson fury"],
+        question: "Who has won the most ballon d'or", difficulty: 1},
+
+    {id: 3, categories: [history, science], answer: "1998", wrong: ["1989", "1994", "2012", "2001"], 
+        question: "When was the ISS launched?", difficulty: 2},
+
+    {id: 4, categories: [science, history], answer: "Eratosthenes of Cyrene", wrong: ["", "", "", ""],
+        question: "Who first proved, that "},
+];
+
 

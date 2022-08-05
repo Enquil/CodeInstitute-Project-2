685802a66b28fae27d4578cc548197f7f80680d0
Jim Olesen <jim.e.olesen@gmail.com>
2022-08-06 00:14:10 +0200

    made thing

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,10 @@ function handleSubmit() {
     event.preventDefault();
 
     let user = document.getElementById("username").value;
-    const greeting = `<h2>Welcome ${user}</h2>`;
-    const categories = `<h3>What would you like to do?</h3>`;
-    const newGame = `<div class="block"><button onclick="meow()"> New Game </button></div>`;
-    const statistics = `<button id="stats">statistics</button>`;
-    let top = document.getElementById("top");
-    let middle = document.getElementById("middle");
-    let bottom = document.getElementById("bottom");
+
+    const top = document.getElementById("top");
+    const middle = document.getElementById("middle");
+    const bottom = document.getElementById("bottom");
 
     while (top.hasChildNodes, middle.hasChildNodes, bottom.hasChildNodes()) {
         top.removeChild(top.firstChild);
@@ -22,16 +19,36 @@ function handleSubmit() {
         bottom.removeChild(bottom.firstChild);
     }
     
-    top.innerHTML = greeting;
-    top.innerHTML += categories;
-    middle.innerHTML = newGame;
-    middle.innerHTML += statistics;
+    top.innerHTML = `<h2>Welcome ${user}</h2>`;
+    top.innerHTML += `<h3>What would you like to do?</h3>`;
+    middle.innerHTML = `<div class="block"><button onclick="newGame()"> New Game </button></div>`;
+    middle.innerHTML += `<button id="stats">statistics</button>`;
 }
 
-function meow() {
-    while (top.hasChildNodes, middle.hasChildNodes, bottom.hasChildNodes()) {
+
+
+function newGame() {
+    const top = document.getElementById("top");
+    const middle = document.getElementById("middle");
+
+    while (top.hasChildNodes, middle.hasChildNodes()) {
         top.removeChild(top.firstChild);
         middle.removeChild(middle.firstChild);
-        bottom.removeChild(bottom.firstChild);
     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+questions {
+
 }

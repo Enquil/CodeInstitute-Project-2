5bd0562872726e38602359300dbb7c36445c9721
Jim Olesen <jim.e.olesen@gmail.com>
2022-08-06 11:07:28 +0200

    added questions and a category selector

--- assets/js/questions1.js
+++
@@ -1,8 +0,0 @@
-let questions = questionList.filter(obj => {
-    return obj.categories = culture;
-  });
-  questions.sort(() => Math.random() - 0.5); 
-  for (let i = 0; i < 5; i++){
-    arr.push(questions[0]);
-    questions.shift();
-}
--- assets/js/script.js
+++ assets/js/script.js
@@ -35,6 +35,13 @@ function newGame() {
         top.removeChild(top.firstChild);
         middle.removeChild(middle.firstChild);
     }
+
+    top.innerHTML = `<h2>Pick a category</h2>`;
+    middle.innerHTML = `<div>
+                            <button onclick="runGame(category)"> Geography </button> 
+                            <button onclick="runGame(category)"> Science </button> 
+                            <button onclick="runGame(category)"> Sports </button>
+                        </div>`;
 }
 
 
@@ -49,6 +56,156 @@ function newGame() {
 
 
 
-questions {
 
-}
+
+questions = [
+    {
+        id: 1, category: "geography", difficulty: 1,
+        question: "By landarea, which country is the largest?", answer: "Russia", wrong: ["Canada", "Australia", "United States", "Germany", "China"]  
+    },
+
+    {
+        id: 2, category: "geography", difficulty: 1,
+        question: "By landarea, which country is the smallest?", answer: "Vatican City", wrong: ["Monaco", "Gibraltar", "Tokelau", "Nauru", "Macao"]
+    },
+
+    {
+        id: 3, category: "geography", difficulty: 2,
+        question: "What is the largest desert in the world", answer: "Antarctic Desert", wrong: ["Sahara Desert", "Arctic Desert", "Great Australian Desert", "Gobi Desert", "Arabian Desert"]
+    },
+
+    {
+        id: 4, category: "geography", difficulty: 3,
+        question: "What percentage of the worlds population lives in the norther hemisphere?", answer: "90%", wrong: ["80%", "85%", "75%", "70%", "87%", "82%"]
+    },
+
+    {
+        id: 5, category: "geography", difficulty: 2,
+        question: "How many timezones does Russia span?", answer: ""
+    },
+
+    {
+        id: 6,
+        category: "geography"
+    },
+
+    {
+        id: 7,
+        category: "geography"
+    },
+
+    {
+        id: 8,
+        category: "geography"
+    },
+
+    {
+        id: 9,
+        category: "geography"
+    },
+
+    {
+        id: 10,
+        category: "geography"
+    },
+
+    {
+        id: 11,
+        category: "science"
+    },
+
+    {
+        id: 12,
+        category: "science"
+    },
+
+    {
+        id: 13,
+        category: "science"
+    },
+
+    {
+        id: 14,
+        category: "science"
+    },
+
+    {
+        id: 15,
+        category: "science"
+    },
+
+    {
+        id: 16,
+        category: "science"
+    },
+
+    {
+        id: 17,
+        category: "science"
+    },
+
+    {
+        id: 18,
+        category: "science"
+    },
+
+    {
+        id: 19,
+        category: "science"
+    },
+
+    {
+        id: 20,
+        category: "science"
+    },
+
+    {
+        id: 21,
+        category: "sports"
+    },
+
+    {
+        id: 22,
+        category: "sports"
+    },
+
+    {
+        id: 23,
+        category: "sports"
+    },
+
+    {
+        id: 24,
+        category: "sports"
+    },
+
+    {
+        id: 25,
+        category: "sports"
+    },
+
+    {
+        id: 26,
+        category: "sports"
+    },
+
+    {
+        id: 27,
+        category: "sports"
+    },
+
+    {
+        id: 28,
+        category: "sports"
+    },
+
+    {
+        id: 29,
+        category: "sports"
+    },
+
+    {
+        id: 30,
+        category: "sports"
+    },
+];
